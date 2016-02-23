4d9293144b52fc1193e84ed34352474bd18780e3
George Evmenov <altren@gmail.com>
2016-02-23 17:27:41 +0300

    implement basic drag'n'drop logic

---
+++ Assets/DragHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Vector3 startPosition;
+    Transform startParent;
+
+    #region IBeginDragHandler implementation
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        DragHelper.itemBeingDragged = gameObject;
+
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        gameObject.transform.SetParent(DragHelper.topRenderTransform);
+    }
+
+    #endregion
+
+    #region IDragHandler implementation
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    #endregion
+
+    #region IEndDragHandler implementation
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DragHelper.itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == DragHelper.topRenderTransform)
+        {
+            transform.position = startPosition;
+            gameObject.transform.SetParent(startParent);
+
+            // TODO: item dropped to nowhere
+            GetComponent<Image>().sprite = null;
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/DragHelper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragHelper : MonoBehaviour
+{
+    public static Transform topRenderTransform;
+    public static GameObject itemBeingDragged;
+
+    // Use this for initialization
+    void Start()
+    {
+        topRenderTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
